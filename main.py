4fb5fefc88eb281ba6cf08d98deadda7ddc5986b
siberguvenlikhaberler <kostakalikostak@gmail.com>
2026-02-18 14:12:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -457,7 +457,7 @@ class HaberSistemi:
         <div class="archive-links">
 """
         for report in reports:
-            archive_html += f'            <a href="/raporlar/{report["filename"]}.html" class="archive-link">{report["date"]}</a>\n'
+            archive_html += f'            <a href="./raporlar/{report["filename"]}.html" class="archive-link">{report["date"]}</a>\n'
         
         archive_html += """        </div>
     </div>
